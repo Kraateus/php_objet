e3e3e3e9e4f4055213dca2ec5902f7b8732cb3f9
Kraateus <stefan.lebohec@imie.fr>
2018-12-04 11:44:33 +0100

    Création d'une class person avec ses instances

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
 
-var_dump (expression:"coucou");
+/*Askip c'est du code de merde*/
+/*class Person {
+    public $firstname;
+    public $lastname;
+}
+
+    $person1 =new Person();
+    $person1->firstname="john";
+    $person1->lastname="Doe";
+    var_dump($person1);
+
+    $person2 =new Person();
+    $person2->firstname="ololo";
+    $person2->lastname="bipbip";
+    var_dump($person2);    */
+
+/*Version bien*/
+
+    class Person {
+        Private $firstname;
+        Private $lastname;
+        
+        public function setFirstname($pFirstname) {
+            $this->firstname = $pFirstname;
+        }
+        public function setLastname($plastname) {
+            $this->lastname = $plastname;
+        }
+    }
+    
+        $person1 =new Person();
+        $person1->setFirstname("john");
+        $person1->setLastname("john");
+        var_dump($person1);
+    
+        
