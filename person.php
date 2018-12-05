d37814770aef56c2e81d481b8f582f41a051687d
Kraateus <stefan.lebohec@imie.fr>
2018-12-05 09:56:28 +0100

    Mise à jour matin du mardi

---
+++ person.php
@@ -0,0 +1,53 @@
+<?php
+class Person {
+    private $dogs = array(); // Optionnel de faire "= array()"
+    private $firstname;
+    private $lastname;
+    public function __construct(string $pFirstname, string $pLastname) {
+        $this->firstname = $pFirstname;
+        $this->lastname = $pLastname;
+    }
+    public function getFirstname(): string {
+        return $this->firstname;
+    }
+    public function getLastname(): string {
+        return $this->lastname;
+    }
+    public function speak(): string {
+        return "Je suis $this->firstname $this->lastname et mon chien est " . $this->dog->getName();
+    }
+    public function buy(Dog $pDog) {
+        $this->dogs[] = $pDog;
+        // $this->dog->setOwner($this);
+    }
+}
+class Dog {
+    private $owner;
+    private $name;
+    public function __construct(string $pName) {
+        $this->name = $pName;
+    }
+    public function getName(): string {
+        return $this->name;
+    }
+    public function setOwner(Person $pOwner) {
+        $this->owner = $pOwner;
+    }
+    public function speak(): string {
+        return "Mon maitre est " . $this->owner->getFirstname() . " " . $this->owner->getLastname() . " et je suis $this->name";
+    }
+}
+$person = new Person("John", "Doe");
+$dog = new Dog("Johnny");
+$person->buy($dog);
+$dogBoby = new Dog("Boby");
+$person->buy($dogBoby);
+//var_dump($person);
+var_dump($person->speak());
+//var_dump($dog->speak());
+//$dogs = array();
+//
+//$dogs[] = $dog;
+//$dogs[] = $dogBoby;
+//
+//var_dump($dogs);
